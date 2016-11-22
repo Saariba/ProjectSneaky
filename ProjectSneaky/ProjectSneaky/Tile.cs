756b3beeab9e4cb64ecb1d4e144bb40eeb1a8dcc
thomas <thomasreich96@gmail.com>
2016-11-22 15:13:39 +0100

    yo

---
+++ ProjectSneaky/ProjectSneaky/Tile.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSneaky
+{
+    class Tile
+    {
+        Vector2 position;
+        Texture2D texture;
+        int id;
+
+
+        public Tile(Texture2D _texture, Vector2 _position, int _id)
+        {
+            _texture = texture;
+            _position = position;
+            id = _id;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+    }
+
+}
---
+++ ProjectSneaky/ProjectSneaky/tilemap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ProjectSneaky
+{
+    class tilemap
+    {
+        Tile[,] tileMap;
+        int tileSize;
+
+        public tilemap(Texture2D[] textures, Texture2D bitMap, int _tileSize)
+        {
+            tileSize = _tileSize;
+            tileMap = new Tile[bitMap.Width, bitMap.Height];
+
+    
+        }
+
+        private void BuildMap(Texture2D[] textures, Texture2D bitMap)
+        {
+            Color[] colors = new Color[bitMap.Width * bitMap.Height];
+            bitMap.GetData<colors>;
+        }
+    }
+}
