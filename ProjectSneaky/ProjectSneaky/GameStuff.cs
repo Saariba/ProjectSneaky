080836ecec7e0babe1d5b819efe59e28ccb7e866
thomas <thomasreich96@gmail.com>
2016-12-06 18:58:05 +0100

    blah

---
+++ ProjectSneaky/ProjectSneaky/GameStuff.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ProjectSneaky
+{
+    class GameStuff
+    {
+        static GameStuff instance;
+        public Tilemap tileMap;
+        public Player player;
+        
+
+        private GameStuff()
+        {
+
+        }
+
+        public static GameStuff Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new GameStuff();
+
+                return instance;
+            }
+        }
+    }
+}
